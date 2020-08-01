9a1b5d72b9783ae5cd26c9af91be802f19436bfd
Patrick A <akolo@hotmail.com>
2020-08-01 23:45:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,22 +1,19 @@
 require_relative 'config/environment'
-require_relative 'models/PigLatinizer.rb'
+require_relative 'models/piglatinizer.rb'
 
 
 class App < Sinatra::Base
 
  get '/' do
-
    erb :user_input
  end
 
  post '/' do
-
    @Pig_Latin = PigLatinizer.new(params[:user_text])
    erb :results
  end
 
  get '/results' do
-
    erb :results
  end
 
